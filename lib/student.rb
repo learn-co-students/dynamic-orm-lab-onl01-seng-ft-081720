2c8c97ef425f69b0f30df4a1b1b7acc80538716d
ellenupark <ellenupark@gmail.com>
2020-09-17 17:19:39 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
-    def self.table_name
-        self.to_s.downcase.pluralize
-      end
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
 end
